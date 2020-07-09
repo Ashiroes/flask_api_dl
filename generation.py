8a23770dee7bd588d9f168f1b1884c5c0610eb58
Antoine Guestin <antoine@tymate.com>
2020-07-09 15:36:58 +0200

    feat(generation): can generate a song

---
+++ generation.py
@@ -0,0 +1,164 @@
+# =======================================================================================================================
+# GENERATION DE MUSIQUE AVEC LSTM - GENERATION A PARTIR D'UN MODELE
+# =======================================================================================================================
+
+# Desactivation de Tensorflow GPU (sinon crash... )
+import os
+import pickle
+import time
+
+import numpy
+from keras.layers import Activation
+from keras.layers import BatchNormalization as BatchNorm
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import LSTM
+from keras.models import Sequential
+from music21 import instrument, note, stream, chord
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+
+# model_name = 'modeles\LSTM_MUSIQUE-03-4.8107.hdf5'
+
+def preparationSequences(notes, pitchnames, n_vocab):
+    noteVersEntier = dict((note, number) for number, note in enumerate(pitchnames))
+
+    # Taille de la sequence à générer
+    taille_sequence = 100
+    entree_reseau = []
+    sortie = []
+
+    for i in range(0, len(notes) - taille_sequence, 1):
+        sequence_in = notes[i:i + taille_sequence]
+        sequence_out = notes[i + taille_sequence]
+
+        entree_reseau.append([noteVersEntier[char] for char in sequence_in])
+        sortie.append(noteVersEntier[sequence_out])
+
+    n_patterns = len(entree_reseau)
+
+    # Changement du format de l'entrée pour qu'elle corresponde à celui attendu par le réseau LSTM
+    normalisationEntree = numpy.reshape(entree_reseau, (n_patterns, taille_sequence, 1))
+
+    # Nomalisation de l'entrée
+    normalisationEntree = normalisationEntree / float(n_vocab)
+
+    return (entree_reseau, normalisationEntree)
+
+
+def creationDuReseau(network_input, n_vocab, model_name):
+    model = Sequential()
+    model.add(LSTM(
+        512,
+        input_shape=(network_input.shape[1], network_input.shape[2]),
+        recurrent_dropout=0.3,
+        return_sequences=True
+    ))
+    model.add(LSTM(512, return_sequences=True, recurrent_dropout=0.3, ))
+    model.add(LSTM(512))
+    model.add(BatchNorm())
+    model.add(Dropout(0.3))
+    model.add(Dense(256))
+    model.add(Activation('relu'))
+    model.add(BatchNorm())
+    model.add(Dropout(0.3))
+    model.add(Dense(n_vocab))
+    model.add(Activation('softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
+
+    # Chargement des poids
+    model.load_weights(model_name)
+
+    return model
+
+
+def generate_notes(modele, entree_reseau, pitchnames, n_vocab):
+    # Choix d'un séquence au hasard
+    print("    Selection de la sequence")
+    start = numpy.random.randint(0, len(entree_reseau) - 1)
+    print("        " + str(start))
+
+    entierVersNote = dict((number, note) for number, note in enumerate(pitchnames))
+
+    # Selection du pattern
+    pattern = entree_reseau[start]
+    print("        Pattern = " + str(pattern))
+
+    prediction_output = []
+
+    # Generation de 500 notes
+    nbNotes = 100
+    i = 0
+    for note_index in range(nbNotes):
+        print(" Generation note ou accord : " + str(i) + "/" + str(nbNotes))
+
+        prediction_input = numpy.reshape(pattern, (1, len(pattern), 1))
+        prediction_input = prediction_input / float(n_vocab)
+
+        prediction = modele.predict(prediction_input, verbose=0)
+
+        index = numpy.argmax(prediction)
+        result = entierVersNote[index]
+        print("      " + str(result))
+        prediction_output.append(result)
+
+        pattern.append(index)
+        pattern = pattern[1:len(pattern)]
+
+        i = i + 1
+
+    return prediction_output
+
+
+def sauvegardeMidiFile(prediction_output):
+    offset = 0
+    output_notes = []
+
+    for pattern in prediction_output:
+
+        # Si le pattern est un accord
+        if ('.' in pattern) or pattern.isdigit():
+            notes_in_chord = pattern.split('.')
+            notes = []
+            for current_note in notes_in_chord:
+                new_note = note.Note(int(current_note))
+                new_note.storedInstrument = instrument.Piano()
+                notes.append(new_note)
+            new_chord = chord.Chord(notes)
+            new_chord.offset = offset
+            output_notes.append(new_chord)
+
+        # Si le pattern est une note
+        else:
+            new_note = note.Note(pattern)
+            new_note.offset = offset
+            new_note.storedInstrument = instrument.Piano()
+            output_notes.append(new_note)
+
+        # Augmentation de l'offset afin que les notes ne s'empilent pas
+        offset += 0.5
+
+    midi_stream = stream.Stream(output_notes)
+    name = 'generation/output' + str(time.time()) + '.mid'
+    midi_stream.write('midi', fp=name)
+    return (name)
+
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#    APPLICATION
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+def generate_song(name: str, model_name: str) -> str:
+    with open('dataset/' + name + '/notes.txt', 'rb') as fichier:
+        notes = pickle.load(fichier)
+
+    frequencesNotes = sorted(set(item for item in notes))
+    n_vocab = len(set(notes))
+
+    entree_reseau, entree_normalisee = preparationSequences(notes, frequencesNotes, n_vocab)
+
+    modele = creationDuReseau(entree_normalisee, n_vocab, model_name)
+
+    prediction = generate_notes(modele, entree_reseau, frequencesNotes, n_vocab)
+
+    return sauvegardeMidiFile(prediction)
--- server.py
+++ server.py
@@ -1,23 +1,26 @@
-import flask
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_file
+
+from flask_api_dl.generation import *
 
 app = Flask(__name__)
 
 # GLOBAL
 categories = ['Rock', '80s']
-categories_with_models = { 'Rock': 'test', '80s': 'test2' }
+categories_with_models = {'Rock': 'test', '80s': 'test2'}
 
-#DEF
+
+# DEF
 def getList(dict: dict) -> list:
     return list(dict.keys())
 
 
-#FLASK API
+# FLASK API
 
 @app.route('/', methods=['GET'])
 def home():
     return '''<h1>Deep Learning - music </h1>
 <p>A prototype API for generate music with Deep Learning.</p>'''
+
 
 @app.route('/api/v1/categories', methods=['GET'])
 def api_all():
@@ -29,11 +32,26 @@ def api_all():
 def generate_model():
     data = request.json
     category = data['category']
-    print(category)
-    # if category == None:
-    #     #todo
+
+    if category is None:
+        resp = jsonify(error="please enter a category")
+        return resp
+
     resp = jsonify(success=True)
     return resp
+
+
+@app.route('/api/v1/song', methods=['POST'])
+def generate_model():
+    data = request.json
+    category = data['category']
+    if category is None:
+        resp = jsonify(error="please enter a category")
+        return resp
+
+    filename = generate_song(category, categories_with_models[category])
+
+    return send_file(filename_or_fp=filename, mimetype="audio/midi", as_attachment=True)
 
 
 app.run(debug=True, port=5000)
